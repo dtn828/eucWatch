0591055875c5ee44653b42ec843b2aa74196b1be
dtn828 <89389245+dtn828@users.noreply.github.com>
2024-04-13 16:12:14 +0300

    Update handler_btn.js

--- v2/handler/handler_btn.js
+++ v2/handler/handler_btn.js
@@ -1,5 +1,5 @@
 //button 
-E.setFlags({pretokenise:1});
+E.setFlags({ pretokenise: 1 });
 /*
 ew.btn=(x)=>{
 	let press;
@@ -21,44 +21,63 @@ ew.btn=(x)=>{
 */
 
 
-function btn1(s){
+function btn1(s) {
 	//"ram";
-	if (this.t1) {clearTimeout(this.t1); this.t1=0;}
-	if (face.offid) {clearTimeout(face.offid);face.offid=0;}
-	if (s.state) { 
-		this.press=true;
-		if (global.euc&&euc.state=="READY"&&2<=euc.dash.live.spd&&euc.dash.opt.horn.en) {euc.wri("hornOn"); if (ew.def.acc) return;}
-		this.t1=setTimeout(() => {
-			this.t1=0;
-			if (global.euc) {
+	if (s.state == this.press) return;
+	this.press = s.state;
+	//if (this.t1) { clearTimeout(this.t1); this.t1 = 0; }
+	
+	if (face.offid) { clearTimeout(face.offid); face.offid = 0; }
+	
+	if (this.press) {
+		this.presscount += 1;
+		if (this.tclick) { clearTimeout(this.tclick); this.tclick = 0; }
+		this.tclick = setTimeout(() => {
+			this.tclick = 0;
+			if (this.presscount === 1 && global.euc && euc.state == "READY" && 2 <= euc.dash.live.spd && euc.dash.opt.horn.en) {
+				if (!euc.is.horn && !this.press) {
+					euc.wri("hornOn");
+					euc.wri("hornOff");
+				} else {
+					euc.wri(this.press || !euc.is.horn ? "hornOn" : "hornOff");
+				}
+				if (ew.def.acc) return;
+			} else if (global.euc && this.press) {
 				euc.tgl();
-				this.press=false;
+			} else if (!this.press && this.presscount === 1) {
+				if (face.pageCurr == -1) {
+					buzzer.nav(buzzer.buzz.on);
+					face.go((global.euc && euc.state != "OFF") ? ew.is.dash[ew.def.dash.face] : face.appCurr, 0);
+				} else if (euc.state != "OFF") {
+					if (face.appCurr.startsWith("dash_")) {
+						acc.isUp = 1;
+						if (ew.tid.acc) changeInterval(ew.tid.acc, 500);
+						face.go(face.appCurr, -1);
+						buzzer.nav(buzzer.buzz.off);
+					} else face.go(ew.is.dash[ew.def.dash.face], 0);
+				} else {
+					if (face.appCurr == "clock") { face.go("clock", -1); buzzer.nav(buzzer.buzz.off); }
+					else face.go("clock", 0);
+				}
+			} else if (this.presscount === 2) {
+				if (global.euc && euc.state == "READY") euc.wri("HLtgl");
+			} else if (this.presscount === 3) {
+				if (ew.def.hid == 1 && ew.is.hidM != "undefined") ew.is.hidM.playpause();
+			} else if (this.presscount === 4) {
+				if (global.euc && euc.state == "READY") euc.wri("strobetgl");
 			}
-		}, process.env.BOARD=="BANGLEJS2"?300:800);
-   }else if (this.press && !s.state)  { 
-		this.press=false;
-		if (global.euc&&euc.state=="READY"&&euc.is.horn&&euc.dash.opt.horn.en) {euc.wri("hornOff"); if (ew.def.acc) return;}
-		if (face.pageCurr==-1) {
-			buzzer.nav(buzzer.buzz.on);
-			face.go((global.euc&&euc.state!="OFF")?ew.is.dash[ew.def.dash.face]:face.appCurr,0);
-		}else if (euc.state!="OFF") { 
-			if (face.appCurr.startsWith("dash_")) {
-				acc.isUp=1;
-				if (ew.tid.acc) changeInterval(ew.tid.acc,500);
-				face.go(face.appCurr,-1);
-				buzzer.nav(buzzer.buzz.off);
-			}else face.go(ew.is.dash[ew.def.dash.face],0);
-		}else {
-			if (face.appCurr=="clock") {face.go("clock",-1); buzzer.nav(buzzer.buzz.off);}
-			else face.go("clock",0); 
-		}
-		
-	}else if (this.press&&global.euc&&euc.state==="READY"&&euc.is.horn&&euc.dash.opt.horn.en) {euc.wri("hornOff"); if (ew.def.acc) return;
-	}else face.off();
+			this.press = false;
+			this.presscount = 0;
+			//if (ew.def.acc) return;
+		}, process.env.BOARD == "BANGLEJS2" ? 300 : 500);
+	} else {
+		if (global.euc && euc.state == "READY" && euc.is.horn && euc.dash.opt.horn.en) { euc.wri("hornOff"); if (ew.def.acc) return; }
+	}
 }
-ew.tid.btn1=setWatch(btn1,BTN1, {repeat:true, debounce:50,edge:0});
-if (process.env.BOARD=="ROCK"){
+this.presscount = 0;
+ew.tid.btn1 = setWatch(btn1, BTN1, { repeat: true, debounce: 20, edge: 0 });
+if (process.env.BOARD == "ROCK") {
 	D46.mode("input_pulldown");
-	btn2=btn1.bind();
-	ew.tid.btn2=setWatch(btn2,D46, {repeat:true, debounce:false,edge:0});
+	btn2 = btn1.bind();
+	ew.tid.btn2 = setWatch(btn2, D46, { repeat: true, debounce: false, edge: 0 });
 }
